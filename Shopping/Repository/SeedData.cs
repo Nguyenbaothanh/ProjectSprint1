93444502b75b634a0084ce174830e1689520f9dc
unknown <thanh2952002@gmail.com>
2024-08-07 13:04:59 +0700

    Up date giao dien tho v2

--- Shopping/Repository/SeedData.cs
+++ Shopping/Repository/SeedData.cs
@@ -16,7 +16,7 @@ namespace Shopping.Repository
 				BrandModel apple = new BrandModel { Name = "Apple", Slug = "apple", Description = "Apple is Large Brand in the world", Status = 1 };
 				BrandModel samsung = new BrandModel { Name = "Samsung", Slug = "Samsung", Description = "Samsung is Large Brand in the world", Status = 1 };
 				_context.Products.AddRange(
-					new ProductModel { Name = "Macbook", Slug = "macbook", Description = "Macbook is Best", Image = "1.jpg", CateGory = macbook, Brand = apple, Price = 1200 },
+					new ProductModel { Name = "Macbook", Slug = "macbook", Description = "Màn hình Liquid Retina XDR 14 inch²\r\nHai cổng Thunderbolt / USB 4, cổng HDMI, khe thẻ nhớ SDXC, jack cắm tai nghe, cổng MagSafe 3\r\nMagic Keyboard với Touch ID\r\nBàn di chuột Force Touch\r\nBộ Tiếp Hợp Nguồn USB-C 70W", Image = "1.jpg", CateGory = macbook, Brand = apple, Price = 1200 },
 					new ProductModel { Name = "Pc", Slug = "pc", Description = "Pc is Best", Image = "1.jpg", CateGory = pc, Brand = samsung, Price = 500 }
 				);
 				_context.SaveChanges();
